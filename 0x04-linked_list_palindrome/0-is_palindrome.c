a900ef9eeedcca71f1d8a05201a4dc1a14b34a6f
AnthonyArmour <anthonydarmour@gmail.com>
2021-12-26 04:42:06 +0000

    pushing linked list palindrome

---
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * helper - checks if a linked list is a palindrome
+ * @left: Starts at head and ends at the middle
+ * @right: Starts at 2nd node in list and ends at last node
+ * Return: 1 if palindrome, 0 if not
+ */
+int helper(listint_t **left, listint_t *right)
+{
+	/* Empty list is a palindrome */
+	if (!right)
+		return (1);
+
+	/* Recursion */
+	if (!is_palindrome_helper(left, right->next))
+		return (0);
+
+	/* Check if left and right nodes are equal */
+	if ((*left)->n != right->n)
+		return (0);
+
+	/* Move left to next node */
+	*left = (*left)->next;
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!head || !*head)
+		return (1);
+
+	return (helper(head, *head));
+}
---
+++ 0x04-linked_list_palindrome/README.md
@@ -0,0 +1,4 @@
+# Linked List Palindrome Algorithm
+
+### [Linked List Palindrome]("Linked List Palindrome")
+A C module for validating whether a singly linked list data structure is a palindrome.
