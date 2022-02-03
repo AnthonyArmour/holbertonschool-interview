027e191ef1b71c1a1c4d98d85e390eda0f4168a0
AnthonyArmour <anthonydarmour@gmail.com>
2022-02-03 23:56:54 +0000

    style fix

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -12,7 +12,9 @@
  */
 void reverse(int *arr, int size)
 {
-    for (int i = 0; i < size/2; i++)
+    int i;
+
+    for (i = 0; i < size/2; i++)
     {
         int temp = arr[i];
         arr[i] = arr[size - 1 - i];
