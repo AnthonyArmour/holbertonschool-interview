1ca543f2e9cce33ceb577c08bbde5e087f4aa466
AnthonyArmour <anthonydarmour@gmail.com>
2022-04-28 16:28:32 +0000

    pushing improved making change

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -6,29 +6,6 @@ coins to make the total ammount.
 """
 
 
-def recurse(coins, total):
-    """
-    Recursive search for change.
-    """
-
-    if len(coins) == 0:
-        return -1
-
-    ctotal, cnt = total, 0
-
-    for coin in coins:
-        if coin <= ctotal:
-            amt = int(ctotal / coin)
-            ctotal -= coin * amt
-            cnt += amt
-
-        if ctotal == 0:
-            return cnt
-
-    coins.pop(0)
-    return recurse(coins, total)
-
-
 def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the
@@ -44,14 +21,49 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    counts = []
+
+    def recurse(coins, total, cnt):
+        """Recursive search for change."""
+
+        if len(coins) == 0:
+            return
+
+        big_coin = coins.pop(0)
+        amt = int(total / big_coin)
+        total -= big_coin * amt
+        cnt += amt
+
+        if total == 0:
+            counts.append(cnt)
+            return
+
+        for i, coin in enumerate(coins):
+            if coin <= total:
+                recurse(coins[i:], total, cnt)
+
+        return
+
+
     coins = sorted(coins, reverse=True)
 
-    return recurse(coins, total)
+    for i, coin in enumerate(coins):
+        if coin <= total:
+            recurse(coins[i:], total, 0)
+
+    # print(counts)
+
+    if len(counts) == 0:
+        return -1
+    else:
+        return min(counts)
 
 
 if __name__ == "__main__":
 
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
-    print(makeChange([1, 2, 10], 11))
-    print(makeChange([1, 4, 5, 10], 1278652))
+    # print(makeChange([1, 2, 25], 37))
+    # print(makeChange([1256, 54, 48, 16, 102], 1453))
+    # print(makeChange([1, 2, 10], 11))
+    print(makeChange([200, 6, 5, 4, 3], 413))
+    print(makeChange([500, 300, 200, 6, 5, 4, 3], 1413))
+    print(makeChange([507, 500, 300, 200, 6, 5, 4, 3], 1413))
