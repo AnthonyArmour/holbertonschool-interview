fb557735ed628f24838525617b8f13038945fbc1
AnthonyArmour <anthonydarmour@gmail.com>
2022-05-19 16:06:24 +0000

    improved prime game

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -15,8 +15,12 @@ who the winner of each game is.
 
 class PrimeGame():
 
-    def __init__(self, n):
-        self.list = list(range(1, n+1))
+    def __init__(self, nums, x):
+        self.list = list(range(1, max(nums)+1))
+        self.nums = sorted(nums)
+        self.x = x
+        #         Maria  Ben
+        self.games = {1: 0, -1: 0}
 
     def isPrime(self, x):
         """Returns True if prime number, else False"""
@@ -32,7 +36,8 @@ class PrimeGame():
         """Removes multiples of x from num list"""
         self.list.pop(self.list.index(x))
         for val in range(x+x, max(self.list)+1, x):
-            self.list.pop(self.list.index(val))
+            if val in self.list:
+                self.list.pop(self.list.index(val))
 
     def makeMove(self):
         """
@@ -47,33 +52,75 @@ class PrimeGame():
                 return False
         return True
 
-    def containsPrime(self):
+    def checkGames(self, turn):
+        """Checks if games have ended, tallies
+        scores, and removes games"""
+        for n in self.nums.copy():
+            if not self.containsPrime(self.nextHighest(n)):
+                self.games[turn] += self.nums.count(n)
+                while n in self.nums:
+                    self.nums.pop(self.nums.index(n))
+            else:
+                break
+
+    def containsPrime(self, lst=None):
         """
         checks for prime in list
         """
-        if len(self.list) == 0:
+        if not lst:
+            lst = self.list
+
+        if len(lst) == 0:
             return False
-        for num in self.list:
+        for num in lst:
             if self.isPrime(num):
                 return True
         return False
 
-    def playGame(self):
+    def nextHighest(self, n):
+        """returns list up to n or
+        up to value less than and closest to n"""
+
+        if n in self.list:
+            return self.list[:self.list.index(n)+1]
+
+        size = len(self.list)
+        low, high = 0, n - 1 if size >= n else size - 1
+        idx = False
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            val = self.list[mid]
+            if val < n:
+                idx = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return self.list[:idx+1]
+
+    def playGames(self):
         """Play single prime game and returns winner"""
 
-        players = {1: "Maria", -1: "Ben"}
         turn = 1
+
+        if 1 in self.nums:
+            self.games[-1] += self.nums.count(1)
+            while 1 in self.nums:
+                self.nums.pop(self.nums.index(1))
 
         if self.containsPrime():
             done = False
         else:
-            return "Ben"
+            self.games[-1] += len(self.nums)
+            done = True
 
         while not done:
             done = self.makeMove()
+            if not done:
+                self.checkGames(turn)
             turn *= -1
-
-        return players[turn]
 
 
 def isWinner(x, nums):
@@ -86,20 +133,35 @@ def isWinner(x, nums):
 
     """
 
-    players = {"Maria": 0, "Ben": 0}
+    game = PrimeGame(nums, x)
+    game.playGames()
+    print(game.games)
 
-    for i in range(x):
-        game = PrimeGame(nums[i])
-        players[game.playGame()] += 1
-
-    if players["Maria"] > players["Ben"]:
+    if game.games[1] > game.games[-1]:
         return "Maria"
-    elif players["Maria"] < players["Ben"]:
+    elif game.games[1] < game.games[-1]:
         return "Ben"
     else:
         return None
 
 
 if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(3, [4, 5, 1])))
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    # print("Winner: {}".format(isWinner(5, [3])))
+    # print("Winner: {}".format(isWinner(3, [2, 5, 10])))
+    # print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+    # print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+    # print("Winner: {}".format(isWinner(4, [11, 30, 1, 7])))
+    # print("Winner: {}".format(isWinner(6, [1, 1, 0, 0, 1, 8])))
+    # print("Winner: {}".format(isWinner(10, [5, 5, 5, 5, 5, 2, 2, 2, 2, 2])))
+
+    nums = [0] * 100
+    for i in range(100):
+        nums[i] = i * i
+
+    print("Winner: {}".format(isWinner(100, nums)))
+
+    nums = [0] * 10000
+    for i in range(10000):
+        nums[i] = i
+
+    print("Winner: {}".format(isWinner(10000, nums)))
